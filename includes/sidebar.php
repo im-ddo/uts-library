9a70b0ac3d9e2cdbff329f402d3a336cf73ff67c
dodo <rivaldoimannuel7@gmail.com>
2025-11-13 00:25:49 +0700

    update: sidebar dan daftar kategori

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -26,7 +26,7 @@
                 </a>
                 <div class="collapse" id="collapsePages" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="daftar_kategori.php">Daftar Kategori</a>
+                        <a class="nav-link" href="">Daftar Kategori</a>
                         <a class="nav-link" href="layout-sidenav-light.html">Tambah Kategori</a>
                     </nav>
                 </div>
