92f7b235fc3f0483723ae8adbf726a17385ca603
dodo <rivaldoimannuel7@gmail.com>
2025-11-13 14:43:42 +0700

    update: sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -15,21 +15,23 @@
                 </a>
                 <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="layout-static.html">Daftar Buku</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Tambah Buku</a>
+                        <a class="nav-link" href="index.php?hal=daftar_buku">Daftar Buku</a>
+                        <a class="nav-link" href="index.php?hal=tambah_buku">Tambah Buku</a>
                     </nav>
                 </div>
+
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                     <div class="sb-nav-link-icon"><i class="fas fa-folder-open"></i></div>
                     Kategori
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <div class="collapse" id="collapsePages" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <div class="collapse <?php echo ($page == 'daftar_kategori' || $page == 'tambah_kategori' || $page == 'edit_kategori') ? 'show' : ''; ?>" id="collapsePages" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="">Daftar Kategori</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Tambah Kategori</a>
+                        <a class="nav-link <?php echo ($page == 'daftar_kategori') ? 'active' : ''; ?>" href="index.php?hal=daftar_kategori">Daftar Kategori</a>
+                        <a class="nav-link <?php echo ($page == 'tambah_kategori') ? 'active' : ''; ?>" href="index.php?hal=tambah_kategori">Tambah Kategori</a>
                     </nav>
                 </div>
+
 
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#colapsAnggota" aria-expanded="false" aria-controls="colapsAnggota">
                     <div class="sb-nav-link-icon"><i class="fas fa-user"></i></div>
