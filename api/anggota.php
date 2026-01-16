c5c746781bd4d145693a92340932cd90ae8483bd
dodo <rivaldoimannuel7@gmail.com>
2026-01-16 10:36:38 +0700

    add: anggota dan buku api

---
+++ api/anggota.php
@@ -0,0 +1,39 @@
+<?php
+
+require "../includes/config.php";
+// Asumsi: $mysqli (koneksi database) tersedia dari config.php
+
+function response($status, $msg, $data = null) {
+    echo json_encode([
+        "status" => $status,
+        "msg" => $msg,
+        "data" => $data
+    ]);
+    exit;
+}
+
+if ($_SERVER['REQUEST_METHOD'] !== "GET") {
+    response("error", "Gunakan method GET.");
+}
+
+$query = $mysqli->query("
+    SELECT id_anggota, nama_lengkap, alamat, foto_profil 
+    FROM anggota 
+    ORDER BY id_anggota DESC
+");
+
+$daftar = [];
+while ($row = $query->fetch_assoc()) {
+    $daftar[] = [
+        "id_anggota"   => $row['id_anggota'],
+        "nama_lengkap" => $row['nama_lengkap'],
+        "alamat"       => $row['alamat'],
+        "foto_profil"  => $row['foto_profil'] 
+            ? "http://192.168.1.18/uts_pdcs/api/anggota.php" . $row['foto_profil']
+            : null
+    ];
+}
+
+response("success", "Berhasil mengambil data anggota", $daftar);
+
+?>
